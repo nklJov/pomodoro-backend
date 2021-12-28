6f3a96ec2abc367d5d2dc2abd0b93d2438d347d1
Nikola Jovicic <njovicic0801@gmai.com>
2021-12-28 22:35:36 +0100

    app origin access

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
