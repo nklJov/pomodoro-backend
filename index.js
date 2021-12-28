6952789ec07cdb1289eac388b8d53ce949afe911
Nikola Jovicic <njovicic0801@gmai.com>
2021-12-28 22:23:28 +0100

    cors fix

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    req.setHeader('Access-Control-Allow-Origin', '*');
-    req.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    req.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
