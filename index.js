9eddf00a0b7b6c70436435e64a4e1ca7ccb22572
Nikola Jovicic <njovicic0801@gmai.com>
2021-12-28 22:18:04 +0100

    fixed error

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    req.setHeader('Access-Control-Allow-Origin', '*');
+    req.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    req.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     next();
 });
